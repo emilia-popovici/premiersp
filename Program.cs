895e9a5beaa806e6a373ff42a4375b55b8503ee1
emilia-popovici <emilia.popovici11@gmail.com>
2026-07-28 20:13:42 +0300

    Mai am un pic. Pana ma spanzur gen

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
