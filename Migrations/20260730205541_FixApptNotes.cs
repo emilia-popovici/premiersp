697ac6f103371376248462edffa2db5ead310f1b
emilia-popovici <emilia.popovici11@gmail.com>
2026-07-30 23:56:06 +0300

    am uitat de migrari

---
+++ Migrations/20260730205541_FixApptNotes.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PremierAuto.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixApptNotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_AspNetUsers_ClientId",
+                table: "Appointments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Appointments",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "Appointments",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_AspNetUsers_ClientId",
+                table: "Appointments",
+                column: "ClientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_AspNetUsers_ClientId",
+                table: "Appointments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Appointments",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "Appointments",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_AspNetUsers_ClientId",
+                table: "Appointments",
+                column: "ClientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
